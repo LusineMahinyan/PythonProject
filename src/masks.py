65d1055fae27ebf1ca629ce5b89cc61bec423b3c
Лусине <lumahinyan@mail.ru>
2025-03-16 15:02:18 +0300

    Сохранение всех изменений

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: int) -> str:
     return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
 
 
-print(get_mask_card_number(7000792289606361))
+# print(get_mask_card_number(7000792289606361))
 
 
 def get_mask_account(account_number: int) -> str:
@@ -13,4 +13,4 @@ def get_mask_account(account_number: int) -> str:
     return "**" + account_number_str[-4:]
 
 
-print(get_mask_account(73654108430135874305))
+# print(get_mask_account(73654108430135874305))
